b6051be3c0413a959c3c1cc11ae357333c5c4f8c
Igor Tkachenko <IATkachenko@gmail.com>
2022-01-20 14:40:17 +0300

    test(sensor): add test for async_setup_entry

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -4,9 +4,13 @@ import logging
 
 from homeassistant.components import sensor
 from homeassistant.const import ATTR_TEMPERATURE
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry
 import pytest
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
+from custom_components.thermal_comfort import async_update_options
+from custom_components.thermal_comfort.const import DOMAIN
 from custom_components.thermal_comfort.sensor import (
     ATTR_FROST_RISK_LEVEL,
     ATTR_HUMIDITY,
@@ -14,7 +18,10 @@ from custom_components.thermal_comfort.sensor import (
     SensorType,
     SimmerZone,
     ThermalPerception,
+    id_generator,
 )
+
+from .const import USER_INPUT
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -670,3 +677,44 @@ async def test_sensor_unavailable(hass, start_ha):
             hass.states.get(f"{TEST_NAME}_{sensor_type}").attributes[ATTR_HUMIDITY]
             == 50.0
         )
+
+
+@pytest.mark.parametrize("_sensor", list(SensorType))
+async def test_update_sensors(hass: HomeAssistant, _sensor: SensorType):
+    """Test sensors update engine.
+
+    When user remove sensor in integration config, then we should remove it from system
+    :param hass: HomeAssistant: Home Assistant object
+    :param _sensor: SensorType: individual sensor for test from variation
+    """
+
+    def get_eid(registry: entity_registry, _id):
+        return registry.async_get_entity_id(
+            domain="sensor", platform=DOMAIN, unique_id=_id
+        )
+
+    er = entity_registry.async_get(hass)
+    data = USER_INPUT.copy()
+    # disable other sensors
+    for s in SensorType:
+        if s != _sensor:
+            data[s] = False
+
+    entry = MockConfigEntry(
+        domain=DOMAIN, data=data, entry_id="test", unique_id="uniqueid"
+    )
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    # Make sure that sensors in registry
+    assert get_eid(er, id_generator(entry.unique_id, _sensor)) is not None
+
+    # Make sure sensors removed from registry
+    data[_sensor] = False
+    entry = MockConfigEntry(
+        domain=DOMAIN, data=data, entry_id="test", unique_id="uniqueid"
+    )
+    assert await async_update_options(hass, entry) is None
+    await hass.async_block_till_done()
+    assert get_eid(er, id_generator(entry.unique_id, _sensor)) is None
